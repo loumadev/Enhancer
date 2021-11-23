5fa807b5eabb86bd55c72d521d6c977e02c95237
C172S <kubo.hresko@gmail.com>
2021-11-23 16:59:09 +0100

    test commit message

--- src/main/java/eu/loumadev/enhancer/Main.java
+++ src/main/java/eu/loumadev/enhancer/Main.java
@@ -9,7 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		this.getLogger().info("The plugin is enabling!");
+		this.getLogger().info("The plugin is enabling...!");
 	}
 
 	@Override
