150090476ca8b97ade23b8f3a5d42d1381ca6187
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2021-11-23 01:04:50 +0100

    Made the project an actual valid plugin

---
+++ src/main/java/eu/loumadev/enhancer/Main.java
@@ -0,0 +1,19 @@
+package eu.loumadev.enhancer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		this.getLogger().info("The plugin is enabling!");
+	}
+
+	@Override
+	public void onDisable() {
+		this.getLogger().info("The plugin is disabling!");
+	}
+}
--- src/test/java/eu/loumadev/enhancer/AppTest.java
+++ src/test/java/eu/loumadev/enhancer/AppTest.java
@@ -12,7 +12,6 @@ public class AppTest {
 	 * Rigorous Test :-)
 	 */
 	@Test
-
 	public void shouldAnswerWithTrue() {
 		assertTrue(true);
 	}
