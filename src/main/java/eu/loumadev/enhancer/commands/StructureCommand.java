72ca73e43047421fc8112272981ca31fa7093ac8
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2021-12-01 12:01:08 +0100

    Added dir generating if the dir does not exist yet

--- src/main/java/eu/loumadev/enhancer/commands/StructureCommand.java
+++ src/main/java/eu/loumadev/enhancer/commands/StructureCommand.java
@@ -28,35 +28,43 @@ public class StructureCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
-        if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            if (player.isOp()) {
-                try {
-                    if(strings[0].equalsIgnoreCase("save")){
-                        if(player.hasMetadata("pos1") && player.hasMetadata("pos2")){
-                            Block pos1 = (Block) player.getMetadata("pos1").get(0).value();
-                            Block pos2 = (Block) player.getMetadata("pos2").get(0).value();
-                            Structure.save(pos1, pos2, new File(this.plugin.getDataFolder(), "structures/" + strings[1] + ".json"));
-                            player.sendMessage("Saved successfully!");
-                        }
-                        else player.sendMessage(ChatColor.RED + "Positions not set");
-                    }
+        if(commandSender instanceof Player) {
+            Player player = (Player)commandSender;
 
-                    else if(strings[0].equalsIgnoreCase("build")){
+            if(player.isOp()) {
+                try {
+
+                    if(strings[0].equalsIgnoreCase("save")) {
+
+                        if(player.hasMetadata("pos1") && player.hasMetadata("pos2")) {
+                            Block pos1 = (Block)player.getMetadata("pos1").get(0).value();
+                            Block pos2 = (Block)player.getMetadata("pos2").get(0).value();
+
+                            File dir = new File(plugin.getDataFolder() + File.separator + "structures");
+                            if(!dir.exists()) dir.mkdir();
+
+                            Structure.save(pos1, pos2, new File(dir, strings[1] + ".json"));
+                            player.sendMessage("Saved successfully!");
+
+                        } else player.sendMessage(ChatColor.RED + "Positions not set");
+
+                    } else if(strings[0].equalsIgnoreCase("build")) {
+
                         Structure.build(player.getLocation(), new File(this.plugin.getDataFolder(), "structures/" + strings[1] + ".json"));
                         player.sendMessage("Built!");
+
                     }
-                } catch (IndexOutOfBoundsException e) {
+
+                } catch(IndexOutOfBoundsException e) {
                     player.sendMessage(ChatColor.RED + command.getUsage());
-                } catch (WorldMismatchException e) {
+                } catch(WorldMismatchException e) {
                     player.sendMessage(ChatColor.RED + e.getMessage());
-                } catch (Exception e) {
+                } catch(Exception e) {
                     e.printStackTrace();
                 }
-            }
-            else player.sendMessage(ChatColor.RED + "You don't have access to this command.");
-        }
-        else logger.log(Level.INFO, "You are not a player!");
+
+            } else player.sendMessage(ChatColor.RED + "You don't have access to this command.");
+        } else logger.log(Level.INFO, "You are not a player!");
 
         return true;
     }
