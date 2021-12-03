39c00dd0fb118ebc9c173d9440e0eedad656c8bb
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2021-12-03 15:07:16 +0100

    Added random generator as a field in class

--- src/main/java/eu/loumadev/enhancer/events/StructureGenerator.java
+++ src/main/java/eu/loumadev/enhancer/events/StructureGenerator.java
@@ -16,41 +16,39 @@ import java.util.logging.Level;
 public class StructureGenerator implements Listener {
 
 	private Plugin plugin;
+	private Random rng;
 
 	public StructureGenerator(Plugin plugin) {
 		this.plugin = plugin;
+		this.rng = new Random();
 	}
 
 	@EventHandler
 	public void onGenerate(final ChunkLoadEvent e) {
-		this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable() {
-			@Override
-			public void run() {
-				if(e.isNewChunk()) {
+		this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, () -> {
+			if(e.isNewChunk()) {
 
-					Chunk chunk = e.getChunk();
-					Biome biome = chunk.getBlock(0, 0, 0).getBiome();
+				Chunk chunk = e.getChunk();
+				Biome biome = chunk.getBlock(0, 0, 0).getBiome();
 
-					if(biome == Biome.FOREST || biome == Biome.FLOWER_FOREST) {
-						Random rand = new Random();
-						if(rand.nextDouble() < 0.25) {
-							int x = rand.nextInt(16);
-							int z = rand.nextInt(16);
+				if(biome == Biome.FOREST || biome == Biome.FLOWER_FOREST) {
+					if(this.rng.nextDouble() < 0.25) {
+						int x = this.rng.nextInt(16);
+						int z = this.rng.nextInt(16);
 
-							for(int y = 255; y >= 0; y--) {
-								if(chunk.getBlock(x, y, z).getType() == Material.GRASS_BLOCK) {
-									try {
-										Structure.build(
-											chunk.getBlock(x, y + 1, z).getLocation(),
-											new File(plugin.getDataFolder(), "/structures/fallen_log.json")
-										);
-										//plugin.getLogger().log(Level.INFO, "Generated a fallen log on " + chunk.getBlock(x,y+1,z));
-									}
-									catch(Exception e) {
-										e.printStackTrace();
-									}
-									break;
+						for(int y = 255; y >= 0; y--) {
+							if(chunk.getBlock(x, y, z).getType() == Material.GRASS_BLOCK) {
+								try {
+									Structure.build(
+										chunk.getBlock(x, y + 1, z).getLocation(),
+										new File(plugin.getDataFolder(), "/structures/fallen_log.json")
+									);
+									//plugin.getLogger().log(Level.INFO, "Generated a fallen log on " + chunk.getBlock(x,y+1,z));
+								} catch(Exception err) {
+									err.printStackTrace();
 								}
+
+								break;
 							}
 						}
 					}
